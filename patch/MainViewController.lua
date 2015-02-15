3ad367fc690c891a15b7ea8417cd6244c2f927f3
Monkey <monkey@ec2-54-67-1-102.us-west-1.compute.amazonaws.com>
2015-02-15 21:27:28 +0800

    no message

--- patch/MainViewController.lua
+++
@@ -1,9 +0,0 @@
-waxClass{"MainViewController", UITableViewController}
-
-function tableView_cellForRowAtIndexPath(self, tableView, indexPath)
-	local cell = self:ORIGtableView_cellForRowAtIndexPath(tableView, indexPath)
-	cell:textLabel():setText("" .. (20 - indexPath:row()))
-	cell:detailTextLabel():setText("Monkey_demo")
-	cell:textLabel():setTextColor(UIColor:redColor())
-	return cell
-end
